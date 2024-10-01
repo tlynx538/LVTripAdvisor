fd353aae33f19d8bc768c0023709c1867aabffd4
Vinayak Jaiwant Mooliyil <vjaiwantx@Vinayaks-MacBook-Air.local>
2024-10-01 18:22:14 -0500

    Little bit of visualisation and extraction of insights

--- main.R
+++ main.R
@@ -4,7 +4,7 @@ data <- read.csv('lasvegas_tripadvisor.csv')
 print(data)
 
 
-# data visualisation 
+## Data Visualisation 
 # display plots for users by country
 country_count_vec <- table(data$User.country)
 
@@ -13,3 +13,19 @@ country_count_vec <- sort(country_count_vec, decreasing=TRUE)
 
 # plotting top 5 countries
 barplot(country_count_vec[1:5])
+
+
+# choose records based on. Traveler Type ("Friends","Business","Families","Solo","Couples")
+traveler_type <- unique(data$Traveler.type)
+
+business <- data[data$Traveler.type==traveler_type[2],]
+
+# Sort by largest score to lowest
+business <- business[order(business$Hotel.stars,decreasing = TRUE),]
+
+
+# Top Hotels preferred by businesses
+business_hotels <- sort(table(business$Hotel.name), decreasing = TRUE)
+
+
+
